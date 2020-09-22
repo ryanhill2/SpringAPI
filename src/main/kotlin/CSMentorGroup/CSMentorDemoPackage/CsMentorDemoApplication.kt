056bd58a9fe21318c3961a3b7fa875c1926fff29
ryan <ryan.hill1@mydit.ie>
2020-09-22 02:28:03 +0100

    starting code from spring initializr

---
+++ src/main/kotlin/CSMentorGroup/CSMentorDemoPackage/CsMentorDemoApplication.kt
@@ -0,0 +1,11 @@
+package CSMentorGroup.CSMentorDemoPackage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsMentorDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CsMentorDemoApplication>(*args)
+}
---
+++ src/test/kotlin/CSMentorGroup/CSMentorDemoPackage/CsMentorDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package CSMentorGroup.CSMentorDemoPackage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsMentorDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
